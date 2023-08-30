3c5a82f1fb0053e22d172a139c01404c07812d2d
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-30 15:19:31 +0100

    Feat: Add children to param

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Header from '@/components/header'
 export default function Home() {
   return (
     <main className='w-full min-h-screen flex flex-col justfiy-start items-stretch px-5'>
-      <div className='w-full flex justify-between items-center gap-10'>
+      <div className='border border-red-500 w-full flex justify-between items-center gap-10'>
         <Header size='xl' />
         <div className='flex justify-start items-center gap-5'>
           <Button reg>Log In</Button>
--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,9 @@
 type HeaderProps = {
   size: 'xxl' | 'xl' | 'l' | 'm'
+  children: React.ReactNode
 }
 
-export default function Header({ size }: HeaderProps) {
+export default function Header({ size, children }: HeaderProps) {
   return (
     <h1
       className={`${size === 'xxl' && 'font-extrabold text-4xl'} ${
@@ -10,6 +11,6 @@ export default function Header({ size }: HeaderProps) {
       } ${size === 'l' && 'font-semibold text-2xl'} ${
         size === 'm' && 'font-medium xl'
       }`}
-    ></h1>
+    >{children}</h1>
   )
 }
