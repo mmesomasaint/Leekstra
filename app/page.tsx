23e8b518a3fb8a0b1398f4b4892abbbdddb64dec
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-30 15:39:40 +0100

    Fix: Increase the logo fontsize

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className='w-full min-h-screen flex flex-col justify-start items-stretch px-5'>
       <div className='w-full flex justify-between items-center gap-10 py-5'>
-        <Header size='xl'>Leekstra</Header>
+        <Header size='xxl'>Leekstra</Header>
         <div className='flex justify-start items-center gap-5'>
           <Button reg>Log In</Button>
           <Button reg primary>
