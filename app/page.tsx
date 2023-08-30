3b17c191d595163e8bccb3d03bafad66a662a7b8
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-30 15:35:10 +0100

    Fix: Make styles responsive

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Header from '@/components/header'
 
 export default function Home() {
   return (
-    <main className='w-full min-h-screen flex flex-col justfiy-start items-stretch px-5'>
-      <div className='border border-red-500 w-full flex justify-between items-center gap-10'>
+    <main className='w-full min-h-screen flex flex-col justify-start items-stretch px-5'>
+      <div className='w-full flex justify-between items-center gap-10 py-5'>
         <Header size='xl'>Leekstra</Header>
         <div className='flex justify-start items-center gap-5'>
           <Button reg>Log In</Button>
