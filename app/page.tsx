9fa8cefd1a87f6841fc1f72fe9febf229408621e
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-30 15:22:17 +0100

    Fix: Add the leekstra text

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className='w-full min-h-screen flex flex-col justfiy-start items-stretch px-5'>
       <div className='border border-red-500 w-full flex justify-between items-center gap-10'>
-        <Header size='xl' />
+        <Header size='xl'>Leekstra</Header>
         <div className='flex justify-start items-center gap-5'>
           <Button reg>Log In</Button>
           <Button reg primary>
