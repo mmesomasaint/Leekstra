f6dda355d642c5d3835c313b9cc77cb9fe5fb32d
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-30 18:00:35 +0100

    Fix: Format with prettier

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -22,42 +22,44 @@ function Page() {
     return router.push('/dashboard')
   }
   return (
-    <div className="min-h-screen w-full flex justify-stretch items-center gap-0 bg-white">
-    <h1 className='mt-44 mb-12 text-4xl font-bold text-black/70 ml-5'>
-      Sign up
-    </h1>
-    <form
-      onSubmit={handleForm}
-      className='flex flex-col justify-start items-start gap-5 ml-5'
-    >
-      <label htmlFor='email'>
-        <p>Email</p>
-        <input
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          type='email'
-          name='email'
-          id='email'
-          placeholder='example@mail.com'
-          className='border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50'
+    <div className='min-h-screen w-full flex justify-stretch items-center gap-0 bg-white'>
+      <h1 className='mt-44 mb-12 text-4xl font-bold text-black/70 ml-5'>
+        Sign up
+      </h1>
+      <form
+        onSubmit={handleForm}
+        className='flex flex-col justify-start items-start gap-5 ml-5'
+      >
+        <label htmlFor='email'>
+          <p>Email</p>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            type='email'
+            name='email'
+            id='email'
+            placeholder='example@mail.com'
+            className='border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50'
+          />
+        </label>
+        <label htmlFor='password'>
+          <p>Password</p>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            type='password'
+            name='password'
+            id='password'
+            placeholder='password'
+            className='border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50'
+          />
+        </label>
+        <Button type='submit'>Sign up</Button>
+      </form>
+      <div className='grow h-full scale-125 rotate-45 origin-top-left overflow-hidden'>
+        <div
+          className={`h-full -scale-125 -rotate-45 w-full bg-[url('/imgs/register-art.jpg)] bg-no-repeat bg-cover bg-center`}
         />
-      </label>
-      <label htmlFor='password'>
-        <p>Password</p>
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          type='password'
-          name='password'
-          id='password'
-          placeholder='password'
-          className='border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50'
-        />
-      </label>
-      <Button type='submit'>Sign up</Button>
-    </form>
-      <div className="grow h-full scale-125 rotate-45 origin-top-left overflow-hidden">
-        <div className={`h-full -scale-125 -rotate-45 w-full bg-[url('/imgs/register-art.jpg)] bg-no-repeat bg-cover bg-center`} />
       </div>
     </div>
   )
