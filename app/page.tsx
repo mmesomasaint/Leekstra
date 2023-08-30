97c6da41b234b2103610d2298d11333f202e25e3
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-30 15:10:01 +0100

    Feat: Create the button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/button";
 import Header from "@/components/header";
 
 export default function Home() {
@@ -5,6 +6,10 @@ export default function Home() {
     <main className='w-full min-h-screen flex flex-col justfiy-start items-stretch px-5'>
       <div className='w-full flex justify-between items-center gap-10'>
         <Header size="xl" />
+        <div className='flex justify-start items-center gap-5'>
+          <Button reg>Log In</Button>
+          <Button reg primary>Register</Button>
+        </div>
       </div>
     </main>
   )
---
+++ components/button.tsx
@@ -0,0 +1,17 @@
+type ButtonProps = {
+  children: React.ReactNode
+  type?: "button" | "submit" | "reset"
+  onClick?: () => void
+  className?: string
+  primary?: boolean
+  disabled?: boolean
+  reg?: boolean
+}
+
+export default function Button({type, onClick, primary, disabled, reg, className, children}: ButtonProps) {
+  return (
+    <button type={type} disabled={disabled} onClick={onClick} className={`${primary ? 'bg-black/75 text-white' : 'bg-white text-black/75'} border border-black/75 shadow-md rounded-md font-medium text-base ${reg && 'px-4 py-2'} ${children}`}>
+      {children}
+    </button>
+  )
+}
