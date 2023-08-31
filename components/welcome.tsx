171cc891671bb47fadb4d6677562234398b89241
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-31 06:35:19 +0100

    Feat: Create the welcome modal

---
+++ components/welcome.tsx
@@ -0,0 +1,11 @@
+export function WelcomePlanner() {
+  return (
+    <div>Welcome, jon doe. To the planner safe haven</div>
+  )
+}
+
+export function WelcomeHost() {
+  return (
+    <div>Welcome, jon doe. To the hosts safe haven</div>
+  )
+}
