281af15ff5ba1e38d7dd97f500867e4cf28003a1
Mmesoma Saint <mmesomasaint@gmail.com>
2023-08-31 06:35:30 +0100

    Fix: Format with prettier

--- components/welcome.tsx
+++ components/welcome.tsx
@@ -1,11 +1,7 @@
 export function WelcomePlanner() {
-  return (
-    <div>Welcome, jon doe. To the planner safe haven</div>
-  )
+  return <div>Welcome, jon doe. To the planner safe haven</div>
 }
 
 export function WelcomeHost() {
-  return (
-    <div>Welcome, jon doe. To the hosts safe haven</div>
-  )
+  return <div>Welcome, jon doe. To the hosts safe haven</div>
 }
