3377a2bbf09c4c08cda3d5deee06b1cbbbf883ae
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-01 21:36:47 +0100

    Feat: Design the filter matches page

---
+++ app/dashboard/host/[id]/filter-matches.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function FilterMatches() {
+  return (
+    <div className="grid grid-cols-3 gap-10">
+      <div className="col-span-2 flex flex-col justify-start items-stretch gap-4 px-10">
+        <div className="flex justify-start items-center gap-10 shadow-md rounded-2xl border border-gray-500/50 p-10">
+          <Image src='/' width={200} height={200} alt='avatar' className="shadow-md" />
+          <span>Name: daniel O'brien</span>
+        </div>
+        <div className="flex justify-start items-center gap-10 shadow-md rounded-2xl border border-gray-500/50 p-10">
+          <Image src='/' width={200} height={200} alt='avatar' className="shadow-md" />
+          <span>Name: daniel O'brien</span>
+        </div>
+        <div className="flex justify-start items-center gap-10 shadow-md rounded-2xl border border-gray-500/50 p-10">
+          <Image src='/' width={200} height={200} alt='avatar' className="shadow-md" />
+          <span>Name: daniel O'brien</span>
+        </div>
+      </div>
+    </div>
+  )
+}
