632fdeab3bceb02201bb56031527e20d3372d051
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-01 21:51:16 +0100

    Fix: Add the header

--- app/dashboard/host/[id]/filter-matches/page.tsx
+++ app/dashboard/host/[id]/filter-matches/page.tsx
@@ -1,7 +1,10 @@
+import Header from '@/components/header'
 import Image from 'next/image'
 
 export default function FilterMatches() {
   return (
+    <div className='px-10'>
+      <Header size='xl'>Profile</Header>
     <div className='grid grid-cols-3 gap-10'>
       <div className='col-span-2 flex flex-col justify-start items-stretch gap-4 px-10'>
         <div className='flex justify-start items-center gap-10 shadow-md rounded-2xl border border-gray-500/50 p-10'>
@@ -38,6 +41,7 @@ export default function FilterMatches() {
       <div className='px-10'>
         <form></form>
       </div>
+    </div>
     </div>
   )
 }
