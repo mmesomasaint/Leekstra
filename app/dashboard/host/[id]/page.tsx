b7ed03aa5591e6bbc104e432de7fdaade4fca929
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-01 21:52:37 +0100

    Fix: Add the header

--- app/dashboard/host/[id]/page.tsx
+++ app/dashboard/host/[id]/page.tsx
@@ -6,6 +6,8 @@ import Image from 'next/image'
 
 export default function Profile() {
   return (
+    <div className='px-10'>
+      <Header size='xl'>Profile</Header>
     <div className='grid grid-cols-3 place-items-stretch gap-10 p-10'>
       <div className='p-5'>
         <Image src='/' width={200} height={200} alt='avatar' />
@@ -21,6 +23,7 @@ export default function Profile() {
           Ratings: 5 stars
         </span>
       </div>
+    </div>
     </div>
   )
 }
