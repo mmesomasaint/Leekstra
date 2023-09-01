e22b7e6089165c26e61b780ae62c1293c9592f64
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-01 20:15:10 +0100

    Feat: Design the link text

---
+++ components/link-text.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+type LinkProps = {
+  href: string 
+  className: string
+  children: React.ReactNode
+}
+
+export default function LinkText({href, className, children}: LinkProps) {
+  return (
+    <div className={`text-blue/75 text-base font-medium hover:underline hover:underline-offset-2 ${className}`}>
+      <Link href={href}>{children}</Link>
+    </div>
+  )
+}
