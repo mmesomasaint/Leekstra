0879c3a99816c68fd428409c715fba327a09238e
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-01 21:49:40 +0100

    Fix: Adjust proposals

--- app/dashboard/host/[id]/proposals/page.tsx
+++ app/dashboard/host/[id]/proposals/page.tsx
@@ -1,11 +1,8 @@
 export default function Proposals() {
   return (
     <div className='grid grid-cols-3 gap-10'>
-    <div className='px-10'>
-      <form></form>
-    </div>
-      <div className='col-span-2 flex flex-col justify-start items-stretch gap-4 px-10'>
-      </div>
+      <div className='px-10'></div>
+      <div className='col-span-2 flex flex-col justify-start items-stretch gap-4 px-10'></div>
     </div>
   )
 }
