f5467c49914794fb41d23788c9316d38057d05c0
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-01 20:15:18 +0100

    Fix: Format with prettier

--- components/link-text.tsx
+++ components/link-text.tsx
@@ -1,14 +1,16 @@
-import Link from "next/link";
+import Link from 'next/link'
 
 type LinkProps = {
-  href: string 
+  href: string
   className: string
   children: React.ReactNode
 }
 
-export default function LinkText({href, className, children}: LinkProps) {
+export default function LinkText({ href, className, children }: LinkProps) {
   return (
-    <div className={`text-blue/75 text-base font-medium hover:underline hover:underline-offset-2 ${className}`}>
+    <div
+      className={`text-blue/75 text-base font-medium hover:underline hover:underline-offset-2 ${className}`}
+    >
       <Link href={href}>{children}</Link>
     </div>
   )
