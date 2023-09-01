dd0f25e32e30a53665a4e70073f8d9457bace916
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-01 21:52:06 +0100

    Fix: Add the header

--- app/dashboard/host/[id]/proposals/page.tsx
+++ app/dashboard/host/[id]/proposals/page.tsx
@@ -1,8 +1,13 @@
+import Header from "@/components/header";
+
 export default function Proposals() {
   return (
+    <div className='px-10'>
+      <Header size='xl'>Profile</Header>
     <div className='grid grid-cols-3 gap-10'>
       <div className='px-10'></div>
       <div className='col-span-2 flex flex-col justify-start items-stretch gap-4 px-10'></div>
+    </div>
     </div>
   )
 }
