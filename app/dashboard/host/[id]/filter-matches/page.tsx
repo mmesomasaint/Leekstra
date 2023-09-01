1222320ab0ac045aad91f051fc8ae5cdbedb5e7b
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-01 21:51:24 +0100

    Fix: Format with prettier

--- app/dashboard/host/[id]/filter-matches/page.tsx
+++ app/dashboard/host/[id]/filter-matches/page.tsx
@@ -5,43 +5,43 @@ export default function FilterMatches() {
   return (
     <div className='px-10'>
       <Header size='xl'>Profile</Header>
-    <div className='grid grid-cols-3 gap-10'>
-      <div className='col-span-2 flex flex-col justify-start items-stretch gap-4 px-10'>
-        <div className='flex justify-start items-center gap-10 shadow-md rounded-2xl border border-gray-500/50 p-10'>
-          <Image
-            src='/'
-            width={200}
-            height={200}
-            alt='avatar'
-            className='shadow-md'
-          />
-          <span>Name: daniel O'brien</span>
+      <div className='grid grid-cols-3 gap-10'>
+        <div className='col-span-2 flex flex-col justify-start items-stretch gap-4 px-10'>
+          <div className='flex justify-start items-center gap-10 shadow-md rounded-2xl border border-gray-500/50 p-10'>
+            <Image
+              src='/'
+              width={200}
+              height={200}
+              alt='avatar'
+              className='shadow-md'
+            />
+            <span>Name: daniel O'brien</span>
+          </div>
+          <div className='flex justify-start items-center gap-10 shadow-md rounded-2xl border border-gray-500/50 p-10'>
+            <Image
+              src='/'
+              width={200}
+              height={200}
+              alt='avatar'
+              className='shadow-md'
+            />
+            <span>Name: daniel O'brien</span>
+          </div>
+          <div className='flex justify-start items-center gap-10 shadow-md rounded-2xl border border-gray-500/50 p-10'>
+            <Image
+              src='/'
+              width={200}
+              height={200}
+              alt='avatar'
+              className='shadow-md'
+            />
+            <span>Name: daniel O'brien</span>
+          </div>
         </div>
-        <div className='flex justify-start items-center gap-10 shadow-md rounded-2xl border border-gray-500/50 p-10'>
-          <Image
-            src='/'
-            width={200}
-            height={200}
-            alt='avatar'
-            className='shadow-md'
-          />
-          <span>Name: daniel O'brien</span>
-        </div>
-        <div className='flex justify-start items-center gap-10 shadow-md rounded-2xl border border-gray-500/50 p-10'>
-          <Image
-            src='/'
-            width={200}
-            height={200}
-            alt='avatar'
-            className='shadow-md'
-          />
-          <span>Name: daniel O'brien</span>
+        <div className='px-10'>
+          <form></form>
         </div>
       </div>
-      <div className='px-10'>
-        <form></form>
-      </div>
-    </div>
     </div>
   )
 }
