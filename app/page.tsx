4166397b547119dcb9116c68a179b2fa14f4cbb1
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-07 03:05:20 +0100

    Fix: Format with prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {MdOutlineArrowForwardIos} from 'react-icons/md'
+import { MdOutlineArrowForwardIos } from 'react-icons/md'
 import { useRouter } from 'next/navigation'
 import Button from '@/components/button'
 import Header from '@/components/header'
@@ -22,21 +22,21 @@ export default function Home() {
         </div>
       </div>
       <div className='grow w-full bg-white'>
-          <section>
-            <div className="h-fit md:h-[40rem] w-full bg-[url('/imgs/hero.jpg')] bg-no-repeat bg-cover bg-center">
-              <div className='flex justify-center items-center h-full w-full bg-black/60'>
-                <div className='w-full h-fit text-center pt-40 md:pt-10 py-16 md:py-0'>
-                  <h1 className='block w-[90%] sm:w-[60%] lg:w-[50%] xl:w-[45%] mx-auto text-4xl md:text-6xl font-bold leading-tight text-white mb-4'>
-                    World's <span className='text-gray-400'>#1 Market</span>,
-                    For Event Hosts & Planners
-                  </h1>
-                  <p className='block w-[90%] sm:w-[80%] md:w-[70%] lg:w-[50%] xl:w-[40%] mx-auto text-lg md:text-2xl lg:text-lg font-normal leading-tight text-white mb-8'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Vivamus tempus dui nec euismod congue. Nulla id libero
-                    dignissim, convallis odio
-                  </p>
-                  <div className='flex justify-center gap-8 items-center'>
-                    <Button reg>
+        <section>
+          <div className="h-fit md:h-[40rem] w-full bg-[url('/imgs/hero.jpg')] bg-no-repeat bg-cover bg-center">
+            <div className='flex justify-center items-center h-full w-full bg-black/60'>
+              <div className='w-full h-fit text-center pt-40 md:pt-10 py-16 md:py-0'>
+                <h1 className='block w-[90%] sm:w-[60%] lg:w-[50%] xl:w-[45%] mx-auto text-4xl md:text-6xl font-bold leading-tight text-white mb-4'>
+                  World's <span className='text-gray-400'>#1 Market</span>, For
+                  Event Hosts & Planners
+                </h1>
+                <p className='block w-[90%] sm:w-[80%] md:w-[70%] lg:w-[50%] xl:w-[40%] mx-auto text-lg md:text-2xl lg:text-lg font-normal leading-tight text-white mb-8'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Vivamus tempus dui nec euismod congue. Nulla id libero
+                  dignissim, convallis odio
+                </p>
+                <div className='flex justify-center gap-8 items-center'>
+                  <Button reg>
                     <span className='flex-shrink-0 flex justify-start items-center gap-1 sm:gap-3'>
                       <span className='text-lg xs:text-xl lg:text-lg font-medium leading-none'>
                         Explore More
@@ -45,14 +45,16 @@ export default function Home() {
                         <MdOutlineArrowForwardIos className='text-lg xs:text-xl z-20' />
                       </span>
                     </span>
-                    </Button>
-                    <Button reg primary>Create Account</Button>
-                  </div>
+                  </Button>
+                  <Button reg primary>
+                    Create Account
+                  </Button>
                 </div>
               </div>
             </div>
-          </section>
-          {/*End of showcase section*/}
+          </div>
+        </section>
+        {/*End of showcase section*/}
       </div>
     </main>
   )
