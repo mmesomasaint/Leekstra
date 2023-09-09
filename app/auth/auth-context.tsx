d0e73258fe7d68faa7977a7549ba0934377b867f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-09 15:25:09 +0100

    Fix: Create the user context

---
+++ app/auth/auth-context.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { createContext, useContext, useState, useEffect } from 'react'
+import { onAuthStateChanged, getAuth, User } from 'firebase/auth'
+import firebase_app from '@/lib/firebase'
+
+const auth = getAuth(firebase_app)
+
+type AuthContextType = {
+  user: User | null
+}
+
+export const AuthContext = createContext<AuthContextType>({user: null})
+
+export const useAuthContext = () => useContext(AuthContext)
+
+export const AuthContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode
+}) => {
+  const [user, setUser] = useState<User | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user)
+      } else {
+        setUser(null)
+      }
+      setLoading(false)
+    })
+
+    return () => unsubscribe()
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {loading ? <div>Loading...</div> : children}
+    </AuthContext.Provider>
+  )
+}
