9fa3737337b3cbee15d2be5b9aa808905b811f08
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-09 16:10:47 +0100

    Fix: Typo

--- app/host/[id]/find-match/page.tsx
+++ app/host/[id]/find-match/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 export default function FilterMatches() {
   return (
     <div className='px-10'>
-      <Header size='xl'>Profile</Header>
+      <Header size='xl'>Best Match</Header>
       <div className='grid grid-cols-3 gap-10'>
         <div className='col-span-2 flex flex-col justify-start items-stretch gap-4 px-10'>
           <div className='flex justify-start items-center gap-10 shadow-md rounded-2xl border border-gray-500/50 p-10'>
