493ac6bc1539c9f0effe211d7bbdcf98fcde0ef2
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-09 16:10:04 +0100

    Fix: Typo

--- app/host/[id]/proposals/page.tsx
+++ app/host/[id]/proposals/page.tsx
@@ -3,7 +3,7 @@ import Header from '@/components/header'
 export default function Proposals() {
   return (
     <div className='px-10'>
-      <Header size='xl'>Profile</Header>
+      <Header size='xl'>Proposals</Header>
       <div className='grid grid-cols-3 gap-10'>
         <div className='px-10'></div>
         <div className='col-span-2 flex flex-col justify-start items-stretch gap-4 px-10'></div>
