d44b1ccb56517ae0c6abc9ebfab6c064fbbb8145
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-11 05:37:16 +0100

    Fix: Format with prettier

--- app/host/[id]/layout.tsx
+++ app/host/[id]/layout.tsx
@@ -2,24 +2,30 @@ import Header from '@/components/header'
 import LinkText from '@/components/link-text'
 import getHost from '@/lib/auth/host/getHost'
 
-export default async function Layout({ authModal, children }: { authModal: React.ReactNode, children: React.ReactNode }) {
+export default async function Layout({
+  authModal,
+  children,
+}: {
+  authModal: React.ReactNode
+  children: React.ReactNode
+}) {
   const user = await getHost()
 
   return (
     <div className='h-full flex flex-col justify-start items-stretch gap-5'>
-    <div className='w-full flex justify-between items-center gap-10 py-5'>
-      <div className='flex gap-10'>
-        <Header size='xxl'>Leekstra</Header>
-        <div className='flex justify-evenly items-center gap-5'>
-          <LinkText href='profile'>Profile</LinkText>
-          <LinkText href='find-match'>Find Match</LinkText>
-          <LinkText href='messages'>Messages</LinkText>
-          <LinkText href='invites'>Jobs</LinkText>
+      <div className='w-full flex justify-between items-center gap-10 py-5'>
+        <div className='flex gap-10'>
+          <Header size='xxl'>Leekstra</Header>
+          <div className='flex justify-evenly items-center gap-5'>
+            <LinkText href='profile'>Profile</LinkText>
+            <LinkText href='find-match'>Find Match</LinkText>
+            <LinkText href='messages'>Messages</LinkText>
+            <LinkText href='invites'>Jobs</LinkText>
+          </div>
         </div>
+        <div className='flex justify-start items-center gap-5'></div>
       </div>
-      <div className='flex justify-start items-center gap-5'></div>
+      <div className='grow'>{user ? children : authModal}</div>
     </div>
-    <div className='grow'>{user ? children : authModal}</div>
-  </div>
   )
 }
