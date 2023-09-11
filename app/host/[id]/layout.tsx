22d88deea2655838ca99dacdb4ca3c3805a9f40d
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-11 05:37:01 +0100

    Fix: Adjust the layout

--- app/host/[id]/layout.tsx
+++ app/host/[id]/layout.tsx
@@ -2,30 +2,24 @@ import Header from '@/components/header'
 import LinkText from '@/components/link-text'
 import getHost from '@/lib/auth/host/getHost'
 
-export default async function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ authModal, children }: { authModal: React.ReactNode, children: React.ReactNode }) {
   const user = await getHost()
 
   return (
-    <>
-    
-    {user ? (
-      
-      <div className='h-full flex flex-col justify-start items-stretch gap-5'>
-      <div className='w-full flex justify-between items-center gap-10 py-5'>
-        <div className='flex gap-10'>
-          <Header size='xxl'>Leekstra</Header>
-          <div className='flex justify-evenly items-center gap-5'>
-            <LinkText href='profile'>Profile</LinkText>
-            <LinkText href='find-match'>Find Match</LinkText>
-            <LinkText href='messages'>Messages</LinkText>
-            <LinkText href='invites'>Jobs</LinkText>
-          </div>
+    <div className='h-full flex flex-col justify-start items-stretch gap-5'>
+    <div className='w-full flex justify-between items-center gap-10 py-5'>
+      <div className='flex gap-10'>
+        <Header size='xxl'>Leekstra</Header>
+        <div className='flex justify-evenly items-center gap-5'>
+          <LinkText href='profile'>Profile</LinkText>
+          <LinkText href='find-match'>Find Match</LinkText>
+          <LinkText href='messages'>Messages</LinkText>
+          <LinkText href='invites'>Jobs</LinkText>
         </div>
-        <div className='flex justify-start items-center gap-5'></div>
       </div>
-      <div className='grow'>{children}</div>
+      <div className='flex justify-start items-center gap-5'></div>
     </div>
-      ) : "Sign in first"}
-      </>
+    <div className='grow'>{user ? children : authModal}</div>
+  </div>
   )
 }
