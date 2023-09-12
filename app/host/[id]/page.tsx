b921a0180824aad89d7cc8836b93f3e1a032b819
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-12 18:40:56 +0100

    Fix: Format with prettier

--- app/host/[id]/page.tsx
+++ app/host/[id]/page.tsx
@@ -5,7 +5,7 @@ import Header from '@/components/header'
 import Image from 'next/image'
 import { redirect } from 'next/navigation'
 
-export const getServerSideProps: GetServerSideProps = async ({params}) => {
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   // Get the currenct user
   const host = await getHost()
 
@@ -15,7 +15,7 @@ export const getServerSideProps: GetServerSideProps = async ({params}) => {
     redirect(`/profile/${params?.query}`)
     return
   }
-  
+
   return { props: { host } }
 }
 
