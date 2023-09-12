a95526ba5269661dbac5113497502046d5ab551a
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-12 16:50:54 +0100

    Fix: Format with prettier

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="absolute inset-0 bg-black/30 flex justify-center items-center">
+    <div className='absolute inset-0 bg-black/30 flex justify-center items-center'>
       Loading...
     </div>
   )
