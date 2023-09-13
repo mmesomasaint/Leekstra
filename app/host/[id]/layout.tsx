e4786e77f9296fa4f0720443364e329fa51fa66c
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-13 06:35:59 +0100

    Fix: Remove context

--- app/host/[id]/layout.tsx
+++ app/host/[id]/layout.tsx
@@ -1,29 +1,27 @@
 import Header from '@/components/header'
 import LinkText from '@/components/link-text'
-import { AuthContextProvider } from '../auth/auth-context'
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
   return (
-    <AuthContextProvider>
-      <div className='h-full flex flex-col justify-start items-stretch gap-5'>
-        <div className='w-full flex justify-between items-center gap-10 py-5'>
-          <div className='flex gap-10'>
-            <Header size='xxl'>Leekstra</Header>
-            <div className='flex justify-evenly items-center gap-5'>
-              <LinkText href='profile'>Profile</LinkText>
-              <LinkText href='find-match'>Find Match</LinkText>
-              <LinkText href='messages'>Messages</LinkText>
-              <LinkText href='invites'>Jobs</LinkText>
-            </div>
+    <div className='h-full flex flex-col justify-start items-stretch gap-5'>
+      <div className='w-full flex justify-between items-center gap-10 py-5'>
+        <div className='flex gap-10'>
+          <Header size='xxl'>Leekstra</Header>
+          <div className='flex justify-evenly items-center gap-5'>
+            <LinkText href='profile'>Profile</LinkText>
+            <LinkText href='find-match'>Find Match</LinkText>
+            <LinkText href='messages'>Messages</LinkText>
+            <LinkText href='invites'>Jobs</LinkText>
           </div>
-          <div className='flex justify-start items-center gap-5'></div>
         </div>
-        <div className='grow'>{children}</div>
+        <div className='flex justify-start items-center gap-5'></div>
       </div>
-    </AuthContextProvider>
+      <div className='grow'>{children}</div>
+    </div>
   )
 }
