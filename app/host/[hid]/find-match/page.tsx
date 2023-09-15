c67cf9106afac938b7e56d25caced83a766fda49
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-15 04:49:45 +0100

    Fix: Design Tweaks

--- app/host/[hid]/find-match/page.tsx
+++ app/host/[hid]/find-match/page.tsx
@@ -7,32 +7,32 @@ export default function FindMatch() {
     <div className='px-10'>
       <Header size='xl'>Best Match</Header>
       <div className='grid grid-cols-3 gap-10'>
-        <div className='col-span-2 flex flex-col justify-start items-stretch gap-4 px-10'>
-          <div className='flex justify-start items-center gap-10 shadow-md rounded-2xl border border-gray-500/50 p-10'>
+        <div className='col-span-2 flex flex-col justify-start items-stretch gap-8 px-10'>
+          <div className='flex justify-start items-center gap-10 shadow-md rounded-2xl bg-gray-400/10 p-10'>
             <Image
               src='/'
-              width={200}
-              height={200}
+              width={150}
+              height={150}
               alt='avatar'
               className='shadow-md'
             />
             <span>Name: daniel O&apos;brien</span>
           </div>
-          <div className='flex justify-start items-center gap-10 shadow-md rounded-2xl border border-gray-500/50 p-10'>
+          <div className='flex justify-start items-center gap-10 shadow-md rounded-2xl bg-gray-400/10 p-10'>
             <Image
               src='/'
-              width={200}
-              height={200}
+              width={150}
+              height={150}
               alt='avatar'
               className='shadow-md'
             />
             <span>Name: daniel O&apos;brien</span>
           </div>
-          <div className='flex justify-start items-center gap-10 shadow-md rounded-2xl border border-gray-500/50 p-10'>
+          <div className='flex justify-start items-center gap-10 shadow-md rounded-2xl bg-gray-400/10 p-10'>
             <Image
               src='/'
-              width={200}
-              height={200}
+              width={150}
+              height={150}
               alt='avatar'
               className='shadow-md'
             />
