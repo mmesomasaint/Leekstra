d694c523ae7703326830e7ee6edc1e7af4c18b9d
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-15 04:00:56 +0100

    Fix: Format with prettier

--- app/host/[hid]/page.tsx
+++ app/host/[hid]/page.tsx
@@ -36,22 +36,23 @@ export default function Profile({ params }: { params: { hid: string } }) {
             Edit Profile
           </Button>
           <div className='flex justify-start items-center gap-10'>
-          <Image
-            src='/imgs/female-avatar.jpg'
-            width={200}
-            height={200}
-            alt='avatar'
-          />
-          <div className='flex flex-col gap-5'><span className='text-lg font-semibold text-black/75'>
-            Name: Emeka D Stalleon
-          </span>
-          <span className='text-lg font-semibold text-black/75'>
-            Email: {host?.email}
-          </span>
-          <span className='text-lg font-semibold text-black/75'>
-            Location: Enugu, Nigeria
-          </span>
-          </div>
+            <Image
+              src='/imgs/female-avatar.jpg'
+              width={200}
+              height={200}
+              alt='avatar'
+            />
+            <div className='flex flex-col gap-5'>
+              <span className='text-lg font-semibold text-black/75'>
+                Name: Emeka D Stalleon
+              </span>
+              <span className='text-lg font-semibold text-black/75'>
+                Email: {host?.email}
+              </span>
+              <span className='text-lg font-semibold text-black/75'>
+                Location: Enugu, Nigeria
+              </span>
+            </div>
           </div>
         </div>
         <div className='p-5'>
