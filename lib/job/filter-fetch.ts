34eaf3b67e8ce490ece6a018ec1ac7b3577daa8e
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-15 19:56:03 +0100

    Fix: Format with prettier

--- lib/job/filter-fetch.ts
+++ lib/job/filter-fetch.ts
@@ -1,18 +1,35 @@
-import { getFirestore, query, where, orderBy, startAfter, collection, getDocs } from "firebase/firestore";
-import { Job } from "./types";
-import firebase_app from "../firebase";
+import {
+  getFirestore,
+  query,
+  where,
+  orderBy,
+  startAfter,
+  collection,
+  getDocs,
+} from 'firebase/firestore'
+import { Job } from './types'
+import firebase_app from '../firebase'
 
 const db = getFirestore(firebase_app)
 const plannersRef = collection(db, 'planners')
 
 export default async function filterFetch(job: Job, afterIdx?: string) {
-  const q = query(plannersRef, where("locationDep", "==", job.locationLocked), where("budgetStartRange", '==', job.budget.from), where("budgetEndRange", "==", job.budget.to), where("pay", "==", job.pay), where("class", "==", job.class), orderBy("id", "asc"), startAfter(afterIdx))
+  const q = query(
+    plannersRef,
+    where('locationDep', '==', job.locationLocked),
+    where('budgetStartRange', '==', job.budget.from),
+    where('budgetEndRange', '==', job.budget.to),
+    where('pay', '==', job.pay),
+    where('class', '==', job.class),
+    orderBy('id', 'asc'),
+    startAfter(afterIdx)
+  )
   const querySnap = await getDocs(q)
-  const docs = querySnap.docs.map(doc => doc.data())
+  const docs = querySnap.docs.map((doc) => doc.data())
 
   // Test.
   querySnap.forEach((doc) => {
-    console.log("A doc: ", doc.data())
+    console.log('A doc: ', doc.data())
   })
 
   return docs
