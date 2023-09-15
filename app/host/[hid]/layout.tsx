e7579b845c62635ccb82787714b744c8a7977af1
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-15 04:12:47 +0100

    Fix: Format with prettier

--- app/host/[hid]/layout.tsx
+++ app/host/[hid]/layout.tsx
@@ -7,24 +7,23 @@ export default async function Layout({
 }: {
   children: React.ReactNode
 }) {
-
   return (
     <AuthContextProvider>
-    <div className='h-full flex flex-col justify-start items-stretch gap-5'>
-      <div className='w-full flex justify-between items-center gap-10 py-5'>
-        <div className='flex gap-10'>
-          <Header size='xxl'>Leekstra</Header>
-          <div className='flex justify-evenly items-center gap-5'>
-            <LinkText href='profile'>Profile</LinkText>
-            <LinkText href='find-match'>Find Match</LinkText>
-            <LinkText href='messages'>Messages</LinkText>
-            <LinkText href='invites'>Jobs</LinkText>
+      <div className='h-full flex flex-col justify-start items-stretch gap-5'>
+        <div className='w-full flex justify-between items-center gap-10 py-5'>
+          <div className='flex gap-10'>
+            <Header size='xxl'>Leekstra</Header>
+            <div className='flex justify-evenly items-center gap-5'>
+              <LinkText href='profile'>Profile</LinkText>
+              <LinkText href='find-match'>Find Match</LinkText>
+              <LinkText href='messages'>Messages</LinkText>
+              <LinkText href='invites'>Jobs</LinkText>
+            </div>
           </div>
+          <div className='flex justify-start items-center gap-5'></div>
         </div>
-        <div className='flex justify-start items-center gap-5'></div>
+        <div className='grow'>{children}</div>
       </div>
-      <div className='grow'>{children}</div>
-    </div>
     </AuthContextProvider>
   )
 }
