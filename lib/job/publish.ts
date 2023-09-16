ff208fb42d5d547483be38aba22492b5195be1c4
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-16 23:55:59 +0100

    Fix: Add title to published job

--- lib/job/publish.ts
+++ lib/job/publish.ts
@@ -5,10 +5,11 @@ import { Job } from './types'
 const db = getFirestore(firebase_app)
 const jobsRef = collection(db, 'jobs')
 
-export default async function publish(job: Job, hostId: string) {
+export default async function publish(job: Job, hostId: string, title: string) {
   try {
     const data = {
       ...job,
+      title,
       hostId,
       invitees: [],
       proposals: [],
