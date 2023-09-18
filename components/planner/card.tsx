b48a93ec94c40febe7b1b9dbaec7a9e53f16e1d6
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-18 20:57:08 +0100

    Feat: Redesign card

--- components/planner/card.tsx
+++ components/planner/card.tsx
@@ -1,20 +1,44 @@
+import {useState} from 'react'
+import invite from '@/lib/job/invite'
 import Image from 'next/image'
+import { DocumentData } from 'firebase/firestore'
+import { Job } from '@/lib/job/types'
+import Button from '../button'
 
-export default function Card() {
+export default function Card({planner, hid, data}: {planner: DocumentData, hid: string, data: Job}) {
+  const [btnText, setBtnText] = useState('Send invite')
+  const [loading, setLoading] = useState(false)
+  const handleInvite = () => {
+    setLoading(true)
+    setBtnText('Sending...')
+
+    invite(hid, planner.id, data).then(() => {
+      setLoading(false)
+      setBtnText('Sent')
+    })
+  }
   return (
-    <div className='flex justify-start items-center gap-10 shadow-md rounded-2xl border border-gray-500/50 p-10'>
+    <div className='flex justify-start items-center gap-10 shadow-md rounded-2xl bg-gray-400/10 p-10'>
       <Image
-        src='/'
-        width={200}
-        height={200}
+        src={planner.photoURL ?? `/imgs/male-avatar.jpg`}
+        width={150}
+        height={150}
         alt='avatar'
         className='shadow-md'
       />
-      <span>Name: daniel O&apos;rien</span>
-      <span>Pay: 5000</span>
-      <span>Budget: from 50,000 to 100,000</span>
-      <span>Location: Nigeria</span>
-      <span>Class: Party</span>
+      <div className='flex flex-col justify-start items-center gap-5'>
+        <div className='flex justify-between items-center gap-10'>
+        <div className='flex flex-col items-start gap-3'>
+        <span>Name: {planner.displayName ?? 'John Doe'}</span>
+        <span>Email: {planner.email ?? 'example@email.com'}</span>
+        <span>Location: Nigeria</span>
+        </div>
+        <Button onClick={() => handleInvite()} reg disabled={loading || (btnText === 'Sent')}>
+          {btnText}
+        </Button>
+        </div>
+        <p>I am a passionate and experienced event planner with over 5 years in the industry. I have a proven track record of planning and executing successful events of all sizes and types, from corporate conferences and trade shows to weddings and social gatherings.</p>
+      </div>
     </div>
-  )
+    )
 }
