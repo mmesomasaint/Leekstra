f19c6c73339a4ff7d0c9b0348db6e223fc7522d6
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-18 19:52:39 +0100

    Feat: Design the exists fn

--- lib/job/exists.ts
+++ lib/job/exists.ts
@@ -1,6 +1,15 @@
 // Return if it exists 
 // return null if it doesn't exist.
 
-// Criteria for checking if it exists:
-// First: Check if any job contains any of the already stated props of the filter data.
-// anything else.
+import { collection, getFirestore, where, query, getDocs } from "firebase/firestore";
+import firebase_app from "../firebase";
+
+const db = getFirestore(firebase_app)
+const jobRef = collection(db, 'jobs')
+
+export default async function exists(hash: string) {
+  const q = query(jobRef, where('hash', '==', hash))
+  const docSnaps = await getDocs(q)
+
+  return docSnaps.docs.shift()
+}
