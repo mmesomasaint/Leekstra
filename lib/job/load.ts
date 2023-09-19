ef2b9c2570397b319acc9d7090b2623b9a478033
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-19 19:10:25 +0100

    Feat: Design the loadAll fn

--- lib/job/load.ts
+++ lib/job/load.ts
@@ -1,9 +1,22 @@
-export async function loadAll() {
-  try {
+import { collection, getDocs, getFirestore, limit, orderBy, query, where } from "firebase/firestore";
+import firebase_app from "../firebase";
 
+const db = getFirestore(firebase_app)
+const jobsRef = collection(db, 'jobs')
+
+export async function loadAll() {
+  let jobDocs, error
+
+  try {
+    const q = query(jobsRef, where('type', '==', 'PUBLIC'), orderBy('createdAt', 'asc'), limit(2))
+    const docSnaps = await getDocs(q)
+
+    jobDocs = docSnaps.docs
   } catch(e) {
-    
+    error = e
   }
+
+  return {jobDocs, error}
 }
 
 export async function loadRecent() {
