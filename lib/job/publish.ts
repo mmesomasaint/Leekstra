0991cc6a8fe38e866ff26a3508423ba63e3c53c1
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-19 18:38:28 +0100

    Feat: Don't create new job if already existing

--- lib/job/publish.ts
+++ lib/job/publish.ts
@@ -2,13 +2,16 @@ import firebase_app from '@/lib/firebase'
 import { collection, doc, getFirestore, setDoc } from 'firebase/firestore'
 import { Job } from './types'
 import create from './create'
+import exists from './exists'
 
 const db = getFirestore(firebase_app)
 const jobsRef = collection(db, 'jobs')
 
 export default async function publish(job: Job, hostId: string, title: string) {
   try {
-    const { jobId } = await create(job, hostId, title)
+    // If job already exists don't create a new, otherwise create a new job.
+    const existingJobRef = await exists(job.hash)
+    const jobId = existingJobRef.jobId ?? (await create(job, hostId)).jobId
 
     if (jobId) {
       const newJobRef = doc(jobsRef, jobId)
