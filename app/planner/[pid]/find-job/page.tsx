8d648edf98aff700695f7a4a27191abc41a8ee09
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-19 18:36:04 +0100

    Fix: Format with prettier

--- app/planner/[pid]/find-job/page.tsx
+++ app/planner/[pid]/find-job/page.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import Header from '@/components/header'
 import { DocumentData } from 'firebase/firestore'
 import Card from '@/components/job/card'
@@ -6,13 +6,13 @@ import Card from '@/components/job/card'
 export default function Jobs() {
   const [jobs, setJobs] = useState<DocumentData[]>([])
 
-  const loadBestMatch = async() => {
+  const loadBestMatch = async () => {
     setJobs([])
 
     // Load the best matching jobs and assign them to jobs.
   }
 
-  const loadRecent = async() => {
+  const loadRecent = async () => {
     setJobs([])
 
     // Load the recent jobs and assign them to jobs.
@@ -23,7 +23,9 @@ export default function Jobs() {
       <Header size='xl'>Jobs</Header>
       <div className='grid grid-cols-3 gap-10'>
         <div className='col-span-2 flex flex-col justify-start items-stretch gap-4 px-10'>
-          {jobs.map(jobDoc => <Card jobDoc={jobDoc} />)}
+          {jobs.map((jobDoc) => (
+            <Card jobDoc={jobDoc} />
+          ))}
         </div>
         <div className='px-10'></div>
       </div>
