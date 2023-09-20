d4c109e764fdca5be0baaecd972d95b38aa8a342
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-20 15:49:06 +0100

    Fix: Format with prettier

--- app/planner/[pid]/find-job/job/[jid]/send-proposal/page.tsx
+++ app/planner/[pid]/find-job/job/[jid]/send-proposal/page.tsx
@@ -1,5 +1,5 @@
 import Button from '@/components/button'
-import {useState, FormEvent} from 'react'
+import { useState, FormEvent } from 'react'
 
 export default function SendProposal() {
   const [proposal, setProposal] = useState<string>('')
@@ -11,21 +11,26 @@ export default function SendProposal() {
   }
 
   return (
-    <form onSubmit={sendProposal} className="flex flex-col gap-7 justify-start items-start">
-    <label htmlFor='proposal'>
-      <p>Proposal:</p>
-      <textarea
-        required
-        onChange={(e) => setProposal(e.target.value)}
-        name='proposal'
-        id='proposal'
-        value={proposal}
-        rows={30}
-        placeholder='Write your proposal...'
-        className='w-full border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50'
-      />
-    </label>
-    <Button type='submit' primary reg>Send</Button>
+    <form
+      onSubmit={sendProposal}
+      className='flex flex-col gap-7 justify-start items-start'
+    >
+      <label htmlFor='proposal'>
+        <p>Proposal:</p>
+        <textarea
+          required
+          onChange={(e) => setProposal(e.target.value)}
+          name='proposal'
+          id='proposal'
+          value={proposal}
+          rows={30}
+          placeholder='Write your proposal...'
+          className='w-full border border-zinc-600/50 rounded-xl p-3 focus:outline-blue-500/50'
+        />
+      </label>
+      <Button type='submit' primary reg>
+        Send
+      </Button>
     </form>
   )
 }
