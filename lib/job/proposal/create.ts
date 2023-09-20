a609e519064d184e82ce71e3adb656539723d59f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-20 16:10:15 +0100

    Fix: Create the job/proposal/create.ts file

---
+++ lib/job/proposal/create.ts
@@ -0,0 +1,19 @@
+import firebase_app from "@/lib/firebase";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+
+const db = getFirestore(firebase_app)
+const proposalsRef = collection(db, 'proposals')
+
+export default async function create(pid: string, proposal: string) {
+  let proposalId, error
+  try {
+    const data = {plannerId: pid, letterBody: proposal}
+    const proposalRef = await addDoc(proposalsRef, data)
+
+    proposalId = proposalRef.id
+  } catch(e) {
+    error = e
+  }
+
+  return {proposalId, error}
+}
