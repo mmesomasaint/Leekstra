07ab8037a68fab9afb75bb5ccc7bb07ee0402c78
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-21 05:57:00 +0100

    Fix: Undo rename file

--- app/planner/[pid]/invites/page.tsx
+++ app/planner/[pid]/invites/page.tsx
@@ -1,9 +1,9 @@
 import Header from '@/components/header'
 
-export default function MyJobs() {
+export default function Invites() {
   return (
     <div className='px-10'>
-      <Header size='xl'>My Jobs</Header>
+      <Header size='xl'>Invites</Header>
       <div className='grid grid-cols-3 gap-10'>
         <div className='px-10'></div>
         <div className='col-span-2 flex flex-col justify-start items-stretch gap-4 px-10'></div>
