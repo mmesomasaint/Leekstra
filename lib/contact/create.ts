3bf703e2788a02189d31ea5b03eded577025c026
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-21 16:32:31 +0100

    Fix: Format with prettier

--- lib/contact/create.ts
+++ lib/contact/create.ts
@@ -1,24 +1,24 @@
-import { addDoc, collection, getFirestore } from "firebase/firestore";
-import firebase_app from "../firebase";
-import getHost from "../auth/host/getHost";
-import getPlanner from "../auth/planner/getPlanner";
+import { addDoc, collection, getFirestore } from 'firebase/firestore'
+import firebase_app from '../firebase'
+import getHost from '../auth/host/getHost'
+import getPlanner from '../auth/planner/getPlanner'
 
 const db = getFirestore(firebase_app)
 const contactsRef = collection(db, 'contacts')
 
 export default async function create(forId: string) {
-  let contactId, error 
-  
+  let contactId, error
+
   try {
-    const userDoc = await getHost(forId) ?? await getPlanner(forId)
-    
+    const userDoc = (await getHost(forId)) ?? (await getPlanner(forId))
+
     if (userDoc) {
       const data = {
         forId,
         details: {
           email: userDoc?.email,
-          phoneNumber: userDoc?.phoneNumber
-        }
+          phoneNumber: userDoc?.phoneNumber,
+        },
       }
 
       const contactRef = await addDoc(contactsRef, data)
@@ -28,5 +28,5 @@ export default async function create(forId: string) {
     error = e
   }
 
-  return {contactId, error}
+  return { contactId, error }
 }
