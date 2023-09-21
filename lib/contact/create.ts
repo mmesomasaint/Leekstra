346ce24751d83a6d577b1dde97ad68cc5c37c844
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-21 16:32:16 +0100

    Fix: CReate the contact/create.ts file

---
+++ lib/contact/create.ts
@@ -0,0 +1,32 @@
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import firebase_app from "../firebase";
+import getHost from "../auth/host/getHost";
+import getPlanner from "../auth/planner/getPlanner";
+
+const db = getFirestore(firebase_app)
+const contactsRef = collection(db, 'contacts')
+
+export default async function create(forId: string) {
+  let contactId, error 
+  
+  try {
+    const userDoc = await getHost(forId) ?? await getPlanner(forId)
+    
+    if (userDoc) {
+      const data = {
+        forId,
+        details: {
+          email: userDoc?.email,
+          phoneNumber: userDoc?.phoneNumber
+        }
+      }
+
+      const contactRef = await addDoc(contactsRef, data)
+      contactId = contactRef.id
+    }
+  } catch (e) {
+    error = e
+  }
+
+  return {contactId, error}
+}
