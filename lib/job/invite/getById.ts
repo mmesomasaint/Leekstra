7ddf4336c307b0c37dfc7d3e034af4d0d6800a89
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-21 06:41:51 +0100

    Feat: Create the /job/invite/getById.ts file

---
+++ lib/job/invite/getById.ts
@@ -0,0 +1,20 @@
+import firebase_app from "@/lib/firebase";
+import { collection, doc, getDoc, getFirestore } from "firebase/firestore";
+
+const db = getFirestore(firebase_app)
+const invitesRef = collection(db, 'invites')
+
+export default async function getById(inviteId: string) {
+  let invite, error 
+
+  try {
+    const inviteRef = doc(invitesRef, inviteId)
+    const inviteDoc = await getDoc(inviteRef)
+
+    invite = inviteDoc.data()
+  } catch (e) {
+    error = e
+  }
+
+  return {invite, error}
+}
