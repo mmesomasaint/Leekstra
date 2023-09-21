09c071907e824eabe2452c285b169389187a492d
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-21 05:36:18 +0100

    Fix: Rename invites route to my-jobs

--- app/planner/[pid]/my-jobs/page.tsx
+++ app/planner/[pid]/my-jobs/page.tsx
@@ -1,9 +1,9 @@
 import Header from '@/components/header'
 
-export default function Invites() {
+export default function MyJobs() {
   return (
     <div className='px-10'>
-      <Header size='xl'>Invites</Header>
+      <Header size='xl'>My Jobs</Header>
       <div className='grid grid-cols-3 gap-10'>
         <div className='px-10'></div>
         <div className='col-span-2 flex flex-col justify-start items-stretch gap-4 px-10'></div>
