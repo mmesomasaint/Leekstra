d1127b2e5bc0f2141e5df166c70075e1fa6c853b
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-22 19:07:26 +0100

    Fix: Format with prettier

--- lib/job/proposal/create.ts
+++ lib/job/proposal/create.ts
@@ -4,7 +4,11 @@ import { addDoc, collection, getFirestore } from 'firebase/firestore'
 const db = getFirestore(firebase_app)
 const proposalsRef = collection(db, 'proposals')
 
-export default async function create(jobId: string, plannerId: string, proposal: string) {
+export default async function create(
+  jobId: string,
+  plannerId: string,
+  proposal: string
+) {
   let proposalId, error
   try {
     const data = { jobId, plannerId, letterBody: proposal }
