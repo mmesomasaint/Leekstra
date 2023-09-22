361f365e07f077b155bfb63a34d910ddd466e8dc
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-22 19:42:55 +0100

    Fix: Create the functioning proposal page

--- app/planner/[pid]/proposals/page.tsx
+++ app/planner/[pid]/proposals/page.tsx
@@ -1,10 +1,75 @@
-import CenterText from "@/components/center-text";
+'use client'
+
+import { useState, useEffect } from 'react'
+import Header from '@/components/header'
+import { useAuthContext } from '../../auth/auth-context'
+import { DocumentData } from 'firebase/firestore'
+import getByPlanner from '@/lib/job/proposal/getByPlanner'
+import getById from '@/lib/job/proposal/getById'
+import CenterText from '@/components/center-text'
+import Accept from '@/components/planner/accept'
 
 export default function Proposals() {
+  const { planner } = useAuthContext()
+  const [proposalJob, setProposalJob] = useState<DocumentData>()
+  const [proposals, setProposals] = useState<DocumentData[]>([])
+  const [selectedProposal, setSelectedProposal] = useState<DocumentData>({})
+  const [selectedProposalId, setSelectedProposalId] = useState<string>('')
+  const [loading, setLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    const fetchproposals = async () => {
+      if (planner) {
+        setProposals([])
+
+        const { proposalDocs } = await getByPlanner(planner.uid)
+        setProposals(proposalDocs?.map((doc) => doc.data()) ?? [])
+      }
+    }
+
+    fetchproposals()
+  }, [])
+
+  useEffect(() => {
+    const getSelectedProposal = () => {
+      setLoading(true)
+
+      getById(selectedProposalId).then(async ({ proposal }) => {
+        if (proposal) setSelectedProposal(proposal)
+        setLoading(false)
+      })
+    }
+
+    getSelectedProposal()
+  }, [selectedProposalId])
+
   return (
-    <CenterText>
-      <span>Proposals loading...</span>
-      <span>Wait a sec...</span>
-    </CenterText>
+    <div className='px-10'>
+      <Header size='xl'>Proposals</Header>
+      <div className='grid grid-cols-3 gap-10'>
+        <div className='flex flex-col justify-start items-start gap-7 px-10'>
+          {proposals.map((proposal) => (
+            <div
+              key={proposal.id}
+              className='border-y border-y-black/75 shadow-md py-5 flex justify-around items-center'
+              onClick={() => setSelectedProposalId(proposal.id)}
+            >
+              {proposal.plannerId} &rarr; {proposal.jobId}
+            </div>
+          ))}
+        </div>
+        <div className='col-span-2 px-10'>
+          {loading ? (
+            <CenterText>Loading...</CenterText>
+          ) : (
+            <div className='flex flex-col justify-start items-stretch gap-4'>
+              <span>For: {selectedProposal.jobId}</span>
+              <span>From: {selectedProposal.plannerId}</span>
+              <Accept from={`${planner?.uid}`} to={selectedProposal.plannerId} />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   )
 }
