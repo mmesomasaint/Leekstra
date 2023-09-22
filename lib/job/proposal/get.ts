8a6d3e15e12ef3854fa672cedeee12223240f970
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-22 19:59:46 +0100

    Fix: REfactor the getById and getByHost

---
+++ lib/job/proposal/get.ts
@@ -0,0 +1,69 @@
+import firebase_app from '@/lib/firebase'
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  limit,
+  orderBy,
+  query,
+  where,
+} from 'firebase/firestore'
+
+const db = getFirestore(firebase_app)
+const proposalsRef = collection(db, 'proposals')
+
+export async function getByHost(hostId: string) {
+  let proposalDocs, error
+
+  try {
+    const q = query(
+      proposalsRef,
+      where('hostId', '==', hostId),
+      orderBy('createdAt', 'asc'),
+      limit(5)
+    )
+    const proposalDocSnaps = await getDocs(q)
+
+    if (!proposalDocSnaps.empty) proposalDocs = proposalDocSnaps.docs
+  } catch (e) {
+    error = e
+  }
+
+  return { proposalDocs, error }
+}
+export async function getByPlanner(proposalId: string) {
+  let proposalDocs, error
+
+  try {
+    const q = query(
+      proposalsRef,
+      where('proposalId', '==', proposalId),
+      orderBy('createdAt', 'asc'),
+      limit(5)
+    )
+    const proposalDocSnaps = await getDocs(q)
+
+    if (!proposalDocSnaps.empty) proposalDocs = proposalDocSnaps.docs
+  } catch (e) {
+    error = e
+  }
+
+  return { proposalDocs, error }
+}
+
+export async function getById(proposalId: string) {
+  let proposal, error
+
+  try {
+    const proposalRef = doc(proposalsRef, proposalId)
+    const proposalDoc = await getDoc(proposalRef)
+
+    proposal = proposalDoc.data()
+  } catch (e) {
+    error = e
+  }
+
+  return { proposal, error }
+}
